4f04c54abe36928e1aad6e98099dd4f592d14982
petr <petr.kovinov@gmail.com>
2024-10-04 00:45:29 +0200

    Прическа 2

--- Front/generated/javascript/default/esm/index.esm.js
+++
@@ -1,8 +0,0 @@
-import { getDataConnect, validateArgs } from 'firebase/data-connect';
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'devfacecom',
-  location: 'us-central1'
-};
-
--- Front/generated/javascript/default/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'devfacecom',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
